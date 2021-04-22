0f754894761bbb5e2892099425a35e74ec1ad45d
yuriDuque <yurithielmann83@gmail.com>
2021-04-22 17:06:59 -0300

    enviando projeto

---
+++ rocketseat/nlw/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Olá Mundo!");
+})
+
+app.post("/", (req, res) => {
+   return res.json({message: "Usuario salvo com sucesso!"});
+})
+
+app.listen(3333, () => {console.log("Server is running on port 3333")});
